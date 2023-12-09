ef47411363efe75861068ce2f2648395f6d9a3be
olsa <olsa.muhaxhiri04@gmail.com>
2023-12-09 01:12:52 +0100

    commit

--- login.js
+++ login.js
@@ -1,22 +1,48 @@
-document.addEventListener("DOMContentLoaded",
-function(ngjarja) {
+document.addEventListener("DOMContentLoaded", function () {
     const BtnLogIn = document.getElementById('login-btn');
-   
-    const validate = (ngjarja) => {
-    ngjarja.preventDefault();
-    const username = document.getElementById('userid');
-    const password = document.getElementById('passwordid');
-    
-    if (username.value === "") {
+
+    const validate = (event) => {
+event.preventDefault();
+
+
+const username = document.getElementById('username');
+const password = document.getElementById('password');
+
+if (username.value === "") {
     alert("Ju lutem shtoni perdoruesin.");
-    username.focus();
-    return false;
-    }
-    if (password.value === "") {
-    alert("Ju lutem shtoni Fjalkalimin.");
-    password.focus();
-    return false;
-    }
-    BtnSubmit.addEventListener('click', validate);
-}
-}
+      username.focus();
+      return false;
+     }
+     const usernameValid = (username) => {
+         const usernameRegex = /^[a-z0-9]{1,20}$/;
+         return usernameRegex.test(username.toLowerCase());
+         }
+         
+
+
+     
+     
+if (password.value === "") {
+     alert("Ju lutem shtoni Fjalkalimin.");
+  password.focus();
+     return false;
+     }
+     const passwordValid = (password) => {
+         const passwordRegex = /^(?=[a-z])[a-z0-9]{8,}$/;
+         return passwordRegex.test(password.toLowerCase());
+         }
+
+
+ };
+
+
+        
+
+    
+    BtnLogIn.addEventListener('click', validate);
+    console.log("JavaScript is running!");
+});
+
+    
+   
+    
