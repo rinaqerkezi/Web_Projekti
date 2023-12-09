d3a6da1fb0e2f8edb322c9d7431aca12c1db9494
rinaqerkezi <rq67748@ubt-uni.net>
2023-12-09 21:52:19 +0100

    commit

--- ValidoSignUp.js
+++ ValidoSignUp.js
@@ -1,12 +1,15 @@
-/*document.querySelector('form').addEventListener('submit', validateForm);*/
+document.addEventListener("DOMContentLoaded", function (event) {
+  const signupForm = document.getElementById('signupForm');
 
-let emriRegex = /^[A-Z][a-z]{3,8}$/;
-let emailRegex = /[a-zA-Z.-_]+@[a-z]+\.[a-z]{2,3}$/;
-let mbiemriRegex = /^[a-zA-Z]{2,}(?:\s[a-zA-Z]{1,}'?-?[a-zA-Z]{2,})?$/;
-let passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,10}$/;
-let phoneNumberRegex = /^\d{3}-\d{3}-\d{3}$/;
+  const validateForm = (event) => {
+    event.preventDefault();
 
-function validateForm() {
+    let emriRegex = /^[A-Z][a-z]{2,8}$/;
+    let emailRegex = /[a-zA-Z.-_]+@[a-z]+\.[a-z]{2,3}$/;
+    let mbiemriRegex = /^[a-zA-Z]{2,}(?:\s[a-zA-Z]{1,}'?-?[a-zA-Z]{2,})?$/;
+    let passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,10}$/;
+    let phoneNumberRegex = /^\d{3}-\d{3}-\d{3}$/;
+
     let emriInput = document.getElementById('emri');
     let emriError = document.getElementById('emriError');
     let mbiemriInput = document.getElementById('mbiemri');
@@ -30,38 +33,54 @@ function validateForm() {
     phoneNumberError.innerText = '';
     genderError.innerText = '';
 
+    let isValid = true;
+
+    if (emriInput.value === "") {
+      alert("Ju lutem shtoni perdoruesin.");
+      emriInput.focus();
+      return false;
+    }
+
     if (!emriRegex.test(emriInput.value)) {
-        emriError.innerText = 'Invalid name';
-        return;
+      emriError.innerText = 'Invalid name';
+      isValid = false;
     }
 
-     if (!mbiemriRegex.test(mbiemriInput.value)) {
-        mbiemriError.innerText = 'Invalid last name';
-        return;
+    if (!mbiemriRegex.test(mbiemriInput.value)) {
+      mbiemriError.innerText = 'Invalid last name';
+      isValid = false;
     }
 
-     if (!emailRegex.test(emailInput.value)) {
-        emailError.innerText = 'Invalid email';
-        return;
+    if (!emailRegex.test(emailInput.value)) {
+      emailError.innerText = 'Invalid email';
+      isValid = false;
     }
 
     if (!passwordRegex.test(passwordInput.value)) {
-        passwordError.innerText = 'Invalid password';
-        return;
+      passwordError.innerText = 'Invalid password';
+      isValid = false;
     }
 
     if (passwordInput.value !== confPasswordInput.value) {
-        confPasswordError.innerText = 'Passwords do not match';
-        return;
+      confPasswordError.innerText = 'Passwords do not match';
+      isValid = false;
     }
+
     if (!phoneNumberRegex.test(phoneNumberInput.value)) {
-        phoneNumberError.innerText = 'Invalid phone number';
-        return;
+      phoneNumberError.innerText = 'Invalid phone number';
+      isValid = false;
     }
 
     if (genderSelect.value === '') {
-        genderError.innerText = 'Please select a gender';
-        return;
+      genderError.innerText = 'Please select a gender';
+      isValid = false;
+    }
 
-    alert('Form submitted successfully!');
-}
+    if (isValid) {
+      alert('Form submitted successfully!');
+      
+    }
+  };
+
+  signupForm.addEventListener('submit', validateForm);
+});
