2f91008f73d217281a4b2daaa00f8bfb7ac3bb17
olsa <olsa.muhaxhiri04@gmail.com>
2024-01-26 12:54:52 +0100

    commit

--- login.php
+++ login.php
@@ -23,7 +23,7 @@ if(isset($_POST['submit'])){
       if($row['user_type'] == 'admin'){
 
          $_SESSION['admin_name'] = $row['name'];
-         header('location:dashboard.php');
+         header('location:homeadmin.php');
 
       }elseif($row['user_type'] == 'user'){
 
